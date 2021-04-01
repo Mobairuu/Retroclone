7136190304168e752f4f06adfb32686c78d259c6
Clement Bowe <cbowe023@myfiu.edu>
2021-04-01 18:32:46 -0400

    changed carousel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/header.component';
-import Carousel from './components/carousel'
+import Carousel from './components/carousel/carousel'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Carousel/>
+      <Carousel></Carousel>
     </div>
   );
 }
